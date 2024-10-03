97b856d1b126e7465d446041507e8ce66c6c94f3
samlan24 <muchemionline@gmail.com>
2024-10-03 12:43:08 +0000

    2-hbtn_course.js

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -33,7 +33,7 @@ export default class HolbertonCourse {
 
   set students(value) {
     if (!Array.isArray(value) || value.every((student) => typeof student !== 'string')) {
-      throw new TypeError('Students must be an array');
+      throw new TypeError('Students must be an array of strings');
     }
     this._students = value;
   }
