6cc5aae000f4b0eec4e5af1d1435229e81871cd2
samlan24 <muchemionline@gmail.com>
2024-10-03 13:26:09 +0000

    5-building.js

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,13 +1,12 @@
 export default class Building {
   constructor(sqft) {
-    if (new.target === Building) {
-      throw new TypeError('Cannot construct Building instances directly');
+    if (this.constructor !== Building) {
+      const props = Object.getOwnPropertyNames(this.constructor.prototype);
+      if (!props.find((e) => e === 'evacuationWarningMessage')) {
+        throw new Error('Class extending Building must override evacuationWarningMessage');
+      }
     }
     this._sqft = sqft;
-
-    if (this.evacuationWarningMessage === undefined) {
-      throw new TypeError('Class extending Building must override evacuationWarningMessage');
-    }
   }
 
   get sqft() {
@@ -15,7 +14,7 @@ export default class Building {
   }
 
   set sqft(sqft) {
-    if (typeof sqft !== 'number') {
+    if ((typeof sqft !== 'number') && (sqft instanceof Number)) {
       throw new TypeError('Sqft must be a number');
     }
     this._sqft = sqft;
