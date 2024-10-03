b99d1fc29f0ae2a403766e637327a2de66983f0a
samlan24 <muchemionline@gmail.com>
2024-10-03 12:51:00 +0000

    3-currency.js

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,32 @@
+export default class Currency {
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(code) {
+    if (typeof code !== 'string') {
+      throw new TypeError('Code must be a String');
+    }
+    this._code = code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(name) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a String');
+    }
+    this._name = name;
+  }
+
+  static displayFullCurrency(currency) {
+    return `${currency.name} (${currency.code})`;
+  }
+}
