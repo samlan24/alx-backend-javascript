df0a520bda34e22224f0927c127ad121eb1fe3bd
samlan24 <muchemionline@gmail.com>
2024-10-08 05:19:32 +0000

    9-groceries_list.js

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,11 +1,11 @@
-export default function groceriesList() {
-  const groceries = new Map([
-    ['Apple', 10],
-    ['Tomatoes', 10],
-    ['Pasta', 1],
-    ['Rice', 1],
-    ['Banana', 5],
-  ]);
+const groceriesList = () => {
+  const map = new Map();
+  map.set('Apples', 10);
+  map.set('Tomatoes', 10);
+  map.set('Pasta', 1);
+  map.set('Rice', 1);
+  map.set('Banana', 5);
+  return map;
+};
 
-  return groceries;
-}
+export default groceriesList;
